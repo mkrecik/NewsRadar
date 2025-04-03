76e61ac2359063cbbd5afd33d68ad60961673f5d
majak <majakret@icloud.com>
2025-04-03 14:49:00 +0200

    scrape interia and save to mongodb

---
+++ newspaper/mongodb_connect.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+import json
+
+# URI z danymi logowania i hostem (np. MongoDB Atlas)
+MONGO_URI = "mongodb+srv://user:jkYpB4hwWYBuQx4T@articles.cndzn.mongodb.net/"
+
+client = MongoClient(MONGO_URI)
+db = client["newspaper"]
+collection = db["articles"]
+
+articles_path = r"article/interia.json"
+with open(articles_path, "r", encoding="utf-8") as file:
+    articles_data = json.load(file)
+
+if articles_data:
+    collection.insert_many(articles_data)
+    print(f"Wstawiono {len(articles_data)} artykułów do MongoDB.")
